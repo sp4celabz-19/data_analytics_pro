2ec351fcf13c723312f7845ec9c17a81e9160ad1
Giorgio <puncho_giorgio@inbox.lv>
2025-05-21 15:43:56 +0300

    Homework_01_01 files are added

---
+++ HomeWork_01.py
@@ -0,0 +1,85 @@
+# Task 2
+
+# result_1 = 2 * 3
+# print(f'result_1 is {result_1}')
+# print(f'Type of result_1: {type(result_1)}')
+
+# result_2 = (3 * 3 + 8) / 3
+# print(f'result_2 is {round(result_2, 3)}')
+# print(f'Type of result_2: {type(result_2)}')
+
+# result_3 = 8 // 3
+# print(f'result_3 is {result_3}')
+# print(f'Type of result_3: {type(result_3)}')
+
+# result_4 = 8 % 3
+# print(f'result_4 is {result_4}')
+# print(f'Type of result_4: {type(result_4)}')
+
+# result_5 = 5 ** 2
+# print(f'result_5 is {result_5}')
+# print(f'Type of result_5: {type(result_5)}')
+
+# result_6 = 'Hello' + 'world'
+# print(f'result_6 is {result_6}')
+# print(f'Type of result_6: {type(result_6)}')
+
+# result_7 = 'Python' * 5
+# print(f'result_7 is {result_7}')
+# print(f'Type of result_7: {type(result_7)}')
+
+# result_8 = 5 < 9
+# print(f'result_8 is {result_8}')
+# print(f'Type of result_8: {type(result_8)}')
+
+
+# Task 3
+
+# first_name = 'Georgiy'      
+# last_name = "Pancheshenko"  
+
+# print(first_name)
+# print(last_name)
+
+
+# Task 4
+
+# fav_principle = "In the face of ambiguity, refuse the temptation to guess."
+
+# print(fav_principle[:10])
+# print(fav_principle[2:12])
+# print(fav_principle[-10:])
+# print(fav_principle[::-1])
+# print(fav_principle[::2])
+# print(fav_principle[1::2])
+
+
+# Task 5
+
+# text = """ "I'm getting better at this!" """
+
+# print(text)
+
+
+# Task 6
+
+# text = """This is line one!
+# And this is line two!"""
+
+# print(text)
+
+
+# Task 7
+
+# text = 'Text!'
+# print(len(text))
+
+
+# Task 8
+
+y = -87.7e10   # -(87.7 * 10**10), негативне число
+z = 87.7e-10   # 87.7 * 10**-10, позитивне число
+
+print("y =", y)
+print("z =", z)
+print("y > z:", y > z)   # в данному випадку буде False, тому що "y" менш ніж "z"
